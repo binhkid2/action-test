e5e6be3a7b39d2ff264e620db461a5a8e22fe805
binhkid2 <binhkid2@gmail.com>
2023-04-24 11:33:16 +0700

    first commit

---
+++ routes/index.tsx
@@ -0,0 +1,21 @@
+import { Head } from "$fresh/runtime.ts";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <img
+          src="/logo.svg"
+          class="w-32 h-32"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+       <h1>Hello from github actions</h1>
+        <Counter start={100} />
+      </div>
+    </>
+  );
+}
